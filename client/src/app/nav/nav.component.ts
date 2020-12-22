88373b7b25f5cc8807c4b812e3629d1d31a3a8d6
pycnonotus <anton.golo.fh@gmail.com>
2020-12-22 20:40:22 +0200

    fixed css. idk how it facked up
    Signed-off-by: pycnonotus <anton.golo.fh@gmail.com>

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,66 +1,64 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css'],
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  barLeftX = 0;
-  barWidth = 0;
-  barSavedX = 0;
-  barSavedWidth = 0;
+    barLeftX = 0;
+    barWidth = 0;
+    barSavedX = 0;
+    barSavedWidth = 0;
 
-  constructor(private elRef: ElementRef) {}
+    constructor(private elRef: ElementRef) {}
 
-  private getPositionFormElement(el) {
-    let xPos = 0;
-    let yPos = 0;
+    private getPositionFormElement(el) {
+        let xPos = 0;
+        let yPos = 0;
 
-    while (el) {
-      xPos += el.offsetLeft - el.scrollLeft + el.clientLeft;
-      yPos += el.offsetTop - el.scrollTop + el.clientTop;
+        while (el) {
+            xPos += el.offsetLeft - el.scrollLeft + el.clientLeft;
+            yPos += el.offsetTop - el.scrollTop + el.clientTop;
 
-      el = el.offsetParent;
+            el = el.offsetParent;
+        }
+        return {
+            x: xPos,
+            y: yPos,
+        };
     }
-    return {
-      x: xPos,
-      y: yPos,
-    };
-  }
 
-  onHover(element): void {
-    this.barSavedX = this.barLeftX;
-    this.barSavedWidth = this.barWidth;
-    const el = element.srcElement;
-    const x = this.getPositionFormElement(el).x;
-    this.barLeftX = x;
-    this.barWidth = el.offsetWidth + 0;
-  }
-  onHoverOut(): void {
-    if (this.barSavedWidth && this.barSavedX) {
-      this.barWidth = this.barSavedWidth;
-      this.barLeftX = this.barSavedX;
+    onHover(element): void {
+        this.barSavedX = this.barLeftX;
+        this.barSavedWidth = this.barWidth;
+        const el = element.srcElement;
+        const x = this.getPositionFormElement(el).x;
+        this.barLeftX = x;
+        this.barWidth = el.offsetWidth + 0;
     }
-  }
-  moveBarTo(element) {
+    onHoverOut(): void {
+        if (this.barSavedWidth && this.barSavedX) {
+            this.barWidth = this.barSavedWidth;
+            this.barLeftX = this.barSavedX;
+        }
+    }
+    moveBarTo(element) {
+        const el = element.srcElement;
+        const x = this.getPositionFormElement(el).x;
+        this.barLeftX = x;
+        this.barWidth = el.offsetWidth + 0;
+        this.barSavedWidth = 0;
+        this.barSavedX = 0;
+    }
 
-    const el = element.srcElement;
-    const x = this.getPositionFormElement(el).x;
-    this.barLeftX = x;
-    this.barWidth = el.offsetWidth + 0;
-    this.barSavedWidth = 0;
-    this.barSavedX = 0;
-
-  }
-
-  ngOnInit(): void {
-     setTimeout(() => {
-       const btn = document.querySelector('app-nav a.active');
-       if (btn) {
-         btn.dispatchEvent(new Event('click'));
-         (btn as any).click(); // idk why but  no click on element google plz fix
-       }
-     }, 1000);
-  }
+    ngOnInit(): void {
+        setTimeout(() => {
+            const btn = document.querySelector('app-nav a.active');
+            if (btn) {
+                btn.dispatchEvent(new Event('click'));
+                (btn as any).click(); // idk why but  no click on element google plz fix
+            }
+        }, 1000);
+    }
 }
